28f8bc14ae59a6644db94ddf3698388591e848c2
sitogi <takano162@gmail.com>
2019-08-25 00:35:10 +0900

    追加処理を追加した

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,22 @@ Route::group(['middleware' => ['web']], function () {
     });
 
     Route::post('/book', function(Request $request) {
-        //
+        $validator = Validator::make($request -> all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $book = new Book; // ORM。 books テーブルと関連付ける。
+        $book->title = $request->name;
+        $book->save();
+
+        return redirect('/');
+
     });
 
     Route::delete('/book/{book}', function(Request $book) {
